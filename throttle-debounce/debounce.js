0bcb491156ac1b4adee9d75cb1b1bf18142ed75e
imthelin <243404855@qq.com>
2021-03-17 00:50:55 +0800

    feat: 防抖

---
+++ throttle-debounce/debounce.js
@@ -0,0 +1,34 @@
+/*
+ * @Author: imthelin
+ * @since: 2021-03-16 23:40:07
+ * @lastTime: 2021-03-17 00:50:25
+ * @LastAuthor: Do not edit
+ * @FilePath: /utils/throttle-debounce/debounce.js
+ * @Description: 触发高频事件后n秒内函数只会执行一次，如果n秒内高频事件再次被触发，则重新计算时间
+ */
+
+let showBox = document.getElementById('increase')
+
+// 主要是依赖闭包特性, 能操作计时器 timer
+function debounce(fn, delay) {
+  let timer
+  return function () {
+    clearTimeout(timer)
+    timer = null
+    let args = [...arguments]
+    let self = this
+    timer = setTimeout(() => {
+      fn.apply(self, args)
+    }, delay)
+  }
+}
+
+function addNum(basic = 1) {
+  showBox.innerHTML = Math.random() * basic
+}
+
+let debounceFn = debounce(addNum, 1000)
+
+showBox.addEventListener('click', () => {
+  debounceFn(100)
+}, false)
